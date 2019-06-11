90287a3bfe8a0a089b5fb37b3887dde64f802775
Pedro Renato Mello <pedro.renato.mello@gmail.com>
2019-06-11 08:38:06 -0300

    Adicionado a pasta Insert
    Pasta Insert contem os SQLs de incersao das tabelas

---
+++ Insert/Armazem.sql
@@ -0,0 +1,8 @@
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (8839, 13, 1, 200, 2110, 140, 3);
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (2889, 14, 2, 148, 4855, 130, 2);
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (6646, 15, 3, 126, 9421, 68, 2);
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (679, 10, 4, 139, 6244, 91, 3);
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (9115, 9, 5, 102, 3111, 68, 1);
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (6592, 12, 6, 62, 1605, 10, 5);
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (7980, 15, 7, 176, 5410, 132, 1);
+insert into Armazem (largura, altura, idarmazem_pk, nummaxcontainers, comprimento, lotacaoatual, idunidade_fk) values (5505, 13, 8, 65, 1527, 54, 2);
